a2c010d16a3518383ae2ba0ae8040ea7b49dcec0
Aditia Warda <aditiawardani6@gmail.com>
2023-01-30 09:02:01 +0700

    Suma education app commit

--- lib/suma_education/main_page/screen/book_konten_detail.dart
+++ lib/suma_education/main_page/screen/book_konten_detail.dart
@@ -5,6 +5,7 @@
 import 'dart:convert';
 import 'dart:typed_data';
 import 'dart:async';
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
@@ -15,8 +16,6 @@ import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:page_turn/page_turn.dart';
 import 'package:suma_education/suma_education/main_page/model/book_list_data.dart';
 import 'package:suma_education/suma_education/main_page/model/book_page.dart';
-import 'package:suma_education/suma_education/main_page/ui_part/main_menu.dart';
-
 
 int i = 0;
 String imageURL = 'https://cdn.eurekabookhouse.co.id/ebh/product/all/tubuh.jpg';
@@ -34,6 +33,8 @@ class _MyListScreenState extends State<DetailBukuState> {
   final FlutterTts flutterTts = FlutterTts();
   bool _voiceOver = true;
   List<BookPage> bookPage = [];
+  late AssetsAudioPlayer _assetsAudioPlayer;
+
 
   @override
   initState() {
@@ -44,12 +45,26 @@ class _MyListScreenState extends State<DetailBukuState> {
       });
     }
     _getBookPage();
+
+    _assetsAudioPlayer = AssetsAudioPlayer.newPlayer();
+    openPlayer();
+
     super.initState();
   }
+
+  void openPlayer() async {
+    await _assetsAudioPlayer.open(
+      Audio("assets/sounds/sound_book_1.mp3"),
+      autoStart: true,
+      showNotification: false,
+    );
+  }
+
 
   void _voiceOverSwitch() {
     setState(() {
       _voiceOver = !_voiceOver;
+      _assetsAudioPlayer.playOrPause();
     });
   }
 
@@ -86,11 +101,11 @@ class _MyListScreenState extends State<DetailBukuState> {
       print(dataBookPage);
       for (var i = 0; i < dataBookPage['data'].length; i++) {
         var id = dataBookPage['data'][i]['id'];
-        var id_book = dataBookPage['data'][i]['id_book'];
-        var page_number = dataBookPage['data'][i]['page_number'];
+        var idBook = dataBookPage['data'][i]['id_book'];
+        var pageNumber = dataBookPage['data'][i]['page_number'];
         var image = dataBookPage['data'][i]['image'];
-        var created_at = dataBookPage['data'][i]['created_at'];
-        bookPage.add(BookPage(id, id_book, page_number, image, created_at));
+        var createdAt = dataBookPage['data'][i]['created_at'];
+        bookPage.add(BookPage(id, idBook, pageNumber, image, createdAt));
       }
     } catch (e) {
       print("Error");
@@ -98,114 +113,96 @@ class _MyListScreenState extends State<DetailBukuState> {
     return 'true';
   }
 
-  Future<String>  _getImage() async{
-    String urio = "https://suma.geloraaksara.co.id/uploads/cover_book/"+widget.book.cover;
-
-    var ImageLoad = CachedNetworkImage(
-      placeholder: (context, url) => const CircularProgressIndicator(),
-      imageUrl: "https://suma.geloraaksara.co.id/uploads/cover_book/"+widget.book.cover,
-      height: MediaQuery.of(context).size.height,
-    );
-
-    http.Response response = await http.get(
-      Uri.parse(urio),
-    );
-
-    String _base64 = base64.encode(response.bodyBytes);
-
-    if (ImageLoad!=null){
-      print(_base64);
-    } else (
-        pick = ''
-    );
-
-    return Future.value("Data download successfully");
-
-  }
-
-  void refreshPage(){
-  }
-
   @override
   build(context) {
     var size = MediaQuery.of(context).size;
     final double itemHeight = (size.height - kToolbarHeight - 24) / 2;
     final double itemWidth = size.width / 2;
-    return Scaffold(
-      body:
-      Stack(
-        children: [
-          Container(
-            child: FutureBuilder<String>(
-              future: _getBookPage(), // async work
-              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.waiting: return Text('Loading....');
-                  default:
-                    if (snapshot.hasError)
-                      return Text('Error: ${snapshot.error}');
-                    else
-                      _getBookPage();
-                    return PageTurn(
-                      key: _controller,
-                      backgroundColor: Colors.white,
-                      showDragCutoff: false,
-                      lastPage: Container(child: null),
-                      children: <Widget>[
-                        for (var i = 0; i < bookPage.length; i++)
-                          // if(i==0)...{
-                          //   SizedBox(
-                          //       height: MediaQuery.of(context).size.height,
-                          //       width: MediaQuery.of(context).size.width,
-                          //       child: Container(
-                          //           child: ClipRRect(
-                          //             child: CachedNetworkImage(
-                          //                 placeholder: (context, url) => const CircularProgressIndicator(),
-                          //                 imageUrl: "https://suma.geloraaksara.co.id/uploads/cover_book/"+widget.book.cover,
-                          //                 height: MediaQuery.of(context).size.height,
-                          //                 fit:BoxFit.fitWidth
-                          //             ),
-                          //           )
-                          //       )
-                          //   ),
-                          // } else if(i>0)...{
-                            SizedBox(
-                                height: MediaQuery.of(context).size.height,
-                                width: MediaQuery.of(context).size.width,
-                                child: Container(
-                                    child: ClipRRect(
-                                      child: CachedNetworkImage(
-                                          placeholder: (context, url) => const CircularProgressIndicator(),
-                                          imageUrl: "https://suma.geloraaksara.co.id/uploads/book_page/"+bookPage[i].image,
-                                          height: MediaQuery.of(context).size.height,
-                                          fit:BoxFit.fitWidth
-                                      ),
-                                    )
-                                )
-                            ),
-                          // }
-                      ],
-                    );
-                }
-              },
+    return WillPopScope(
+      onWillPop: () async {
+        new Future.delayed(new Duration(milliseconds: 300), () {
+          Navigator.pop(context);
+        });
+        _assetsAudioPlayer.stop();
+        return false;
+      },
+      child: Scaffold(
+        body:
+        Stack(
+          children: [
+            Container(
+              child: FutureBuilder<String>(
+                future: _getBookPage(), // async work
+                builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting: return Text('Loading....');
+                    default:
+                      if (snapshot.hasError)
+                        return Text('Error: ${snapshot.error}');
+                      else
+                        _getBookPage();
+                      return PageTurn(
+                        key: _controller,
+                        backgroundColor: Colors.white,
+                        showDragCutoff: false,
+                        lastPage: Container(child: null),
+                        children: <Widget>[
+                          for (var i = 0; i < bookPage.length+1; i++)
+                            if(i==0)...{
+                              SizedBox(
+                                  height: MediaQuery.of(context).size.height,
+                                  width: MediaQuery.of(context).size.width,
+                                  child: Container(
+                                      child: ClipRRect(
+                                        child: CachedNetworkImage(
+                                            placeholder: (context, url) => const CircularProgressIndicator(),
+                                            imageUrl: "https://suma.geloraaksara.co.id/uploads/cover_book/"+widget.book.cover,
+                                            height: MediaQuery.of(context).size.height,
+                                            fit:BoxFit.fitWidth
+                                        ),
+                                      )
+                                  )
+                              ),
+                            } else if(i>0)...{
+                              SizedBox(
+                                  height: MediaQuery.of(context).size.height,
+                                  width: MediaQuery.of(context).size.width,
+                                  child: Container(
+                                      child: ClipRRect(
+                                        child: CachedNetworkImage(
+                                            placeholder: (context, url) => const CircularProgressIndicator(),
+                                            imageUrl: "https://suma.geloraaksara.co.id/uploads/book_page/"+bookPage[i-1].image,
+                                            height: MediaQuery.of(context).size.height,
+                                            fit:BoxFit.fitWidth
+                                        ),
+                                      )
+                                  )
+                              ),
+                            }
+                        ],
+                      );
+                  }
+                },
+              ),
             ),
-          ),
-          Container(
-            height: 33,
-            color: Colors.orange,
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.orange,
-        child: Icon(
-          _voiceOver ? Icons.volume_up : Icons
-              .volume_off, color: Colors.white),
-        onPressed: () {
-          _voiceOverSwitch();
-        },
-      ),
+            Container(
+              height: 33,
+              color: Colors.orange,
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.orange,
+          child: Icon(
+              _voiceOver ? Icons.volume_up : Icons
+                  .volume_off, color: Colors.white),
+          onPressed: () {
+            _voiceOverSwitch();
+          },
+        ),
+      )
     );
+
   }
 
 }
